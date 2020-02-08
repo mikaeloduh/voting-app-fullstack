1629dcff09b2ac5cbe3d8ed51348177b09ec8162
mikaeloduh <mikaeloduh@users.noreply.github.com>
2020-02-08 09:57:39 +0800

    refactor(server): separate out app

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const express = require("express");
+
+const authRoute = require("./routes/auth");
+const db = require("./models");
+const errorHandler = require("./api/error");
+const pollsRoute = require("./routes/polls");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/test", function(req, res) {
+  res.status.send('Hello world!');
+});
+
+app.use("/auth", authRoute);
+app.use("/api/polls", pollsRoute);
+
+app.use(function(req, res, next) {
+  let err = new Error("Not found");
+  err.status = 404;
+  next(err);
+});
+app.use(errorHandler);
+
+module.exports = app;
--- src/server.js
+++ src/server.js
@@ -1,37 +1,11 @@
-const express      = require("express");
-const cors         = require('cors');
-const bodyParser   = require('body-parser');
-const dotenv       = require('dotenv');
-const db           = require("./models");
-const authRoute    = require("./routes/auth");
-const pollsRoute   = require("./routes/polls");
-const errorHandler = require("./api/error");
+const dotenv = require('dotenv');
 
-const PORT         = process.env.PORT || 8081;
+const app = require('./app');
+const db = require('./models');
 
-const app = express();
-dotenv.config({verbose: true});
-app.use(cors());
-app.use(bodyParser.json());
+dotenv.config({ verbose: true });
 
-app.get("/ping", function(req, res) {
-  const customers = [
-    {id: 1, firstName: 'John', lastName: 'Doe'},
-    {id: 2, firstName: 'Brad', lastName: 'Traversy'},
-    {id: 3, firstName: 'Mary', lastName: 'Swanson'},
-  ];
-  res.send(customers);
-});
-
-app.use("/auth", authRoute);
-app.use("/api/polls", pollsRoute);
-
-app.use(function(req, res, next) {
-  let err = new Error("Not found");
-  err.status = 404;
-  next(err);
-});
-app.use(errorHandler);
+const PORT = process.env.PORT || 8081;
 
 app.listen(PORT, function() {
   console.log(`Server is starting on port ${PORT}`);
