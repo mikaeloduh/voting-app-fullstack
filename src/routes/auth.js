b0d7b8f7afd931e90ae9b8640b8c8d8949e4e994
mikaeloduh <mikaeloduh@users.noreply.github.com>
2020-02-12 11:31:43 +0800

    docs(auth)(polls): api documentation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,39 @@
 const express = require('express');
+
 const { signup, login } = require("../api/auth")
 
 const router = express.Router();
 
-// Path - /auth ...
+/**
+ * @api {post} /auth/singup Signup a user
+ * 
+ * @apiParam {string} email    User ID (in email format)
+ * @apiParam {string} username User's Name
+ * @apiParam {string} password User's password
+ * 
+ * @apiSuccess (201) {Object} Username and login token
+ * @apiSuccessExample (json) Success-Response:
+ *   {
+ *     "id": "5e4364daac0804d28bbab94c",
+ *     "username": "joe91",
+ *     "token": "eyJhbGciOiJIUzVCJ9.eyJpZCI6IjVlNyUwMzR9.lDXv4B02L9MsnLnvZ0"
+ *   }
+ */
 router.post("/signup", signup);
+
+/**
+ * @api {post} /auth/login User login
+ * 
+ * @apiParam {String} email    User ID (in email format)
+ * @apiParam {String} password User's password
+ * 
+ * @apiSuccess (200) {Object} User's uid and login token
+ * @apiSuccessExample {json} Success-Response:
+ *   {
+ *     "uid": "5e3976db443fcc9bd07f48d8",
+ *     "token": "eyJhbGckpXVCJ9.eyJpZmNDhkOCIsInVzNDc1NjQ.3mOyVC07tM7JEeJk"
+ *   }
+ */
 router.post("/login", login);
 
 module.exports = router;
--- src/routes/polls.js
+++ src/routes/polls.js
@@ -1,14 +1,79 @@
 const express = require('express');
+
 const { createPoll, listAllPolls, getPoll, deletePoll, modifyPoll } = require("../api/polls");
 const { authenticate, authorize } = require("../api/auth");
 
 const router = express.Router();
 
-// Path - /api/polls ...
+/** 
+ * @api {get} /api/polls Fetch a list of all polls
+ * 
+ * @apiSuccess (200) {Object} `Poll` objects List
+ */
 router.get("/", listAllPolls);
+
+/**
+ * @api {post} /api/poll Create a poll
+ * 
+ * @apiParam {String}        topic   The poll topic to create
+ * @apiParam {Array<string>} options Options for this poll
+ * @apiParamExample {json} Request-Example:
+ *   {
+ *     "topic": "How do you like your steak?",
+ *     "options": [
+ *       { "name": "rare", "votes": 0 },
+ *       { "name": "medium", "votes": 0 },
+ *     ]
+ *   }
+ * 
+ * @apiSuccess (201) {Object} The created `Poll` object
+ */
 router.post("/", authenticate, createPoll);
+
+/**
+ * @api {get} /api/poll/:poll_id Retreve a poll detail by `:poll_id`
+ * 
+ * @apiSuccess (200) {String}        _id     `Poll` primary key
+ * @apiSuccess (200) {String}        creater `User`'s object key
+ * @apiSuccess (200) {String}        topic   Poll Title
+ * @apiSuccess (200) {Array<String>} options Array of options
+ * @apiSuccess (200) {String}        .votes  Number of vote to option
+ * @apiSuccess (200) {String}        .option Option name
+ * @apiSuccessExample {json} Success-Response: 
+ *   [
+ *     {
+ *       "_id": "5c00aba4fe60fc1834d3289b",
+ *       "creater": "5bee7cb2a2e7075ef9c2f1c8",
+ *       "topic": "What is your favorite color?",
+ *       "options": [
+ *         {
+ *           "votes": 0,
+ *           "_id": "5c00aba4fe60fc1834d3289e",
+ *           "option": "Red"
+ *         },
+ *         ...
+ *       ]
+ *     },
+ *     ...
+ *   ]
+ */
 router.get("/:poll_id", getPoll);
+
+/**
+ * @api {put} /api/pull/:poll_id Update a poll by `:poll_id`
+ * @permission authenticate
+ * 
+ * @apiParamExample {json} Request-Example: 
+ *   Fields to be update (See @api {post} /api/poll)
+ */
 router.put("/:poll_id", authenticate, modifyPoll);
+
+/**
+ * @api {delete} /api/poll/:poll_id Delete a poll by `:poll_id`
+ * @permission authenticate, authorize
+ * 
+ * @apiSuccess {String} The deleted poll id
+ */
 router.delete("/:poll_id", authenticate, authorize, deletePoll);
 
 module.exports = router;
